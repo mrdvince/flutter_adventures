758827d434eb5eee51f18bb12998e690c597a9e8
Droid <vincivenv@gmail.com>
2019-12-22 21:01:36 +0300

    navigator pop to home screen
    navigate to home screen, add the container inside the stack

--- travel_ui/lib/screens/destinations_screen.dart
+++ travel_ui/lib/screens/destinations_screen.dart
@@ -14,25 +14,40 @@ class _DestinationScreenState extends State<DestinationScreen> {
     return Scaffold(
       body: Column(
         children: <Widget>[
-          Container(
-            height: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30.0),
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.black26,
-                    offset: Offset(0.0, 2.0),
-                    blurRadius: 6.0)
-              ],
-            ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(30.0),
-              child: Image(
-                image: AssetImage(widget.destination.imageUrl),
-                fit: BoxFit.cover,
+          Stack(children: <Widget>[
+            Container(
+              height: MediaQuery.of(context).size.width * 2 / 3,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30.0),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black26,
+                      offset: Offset(0.0, 2.0),
+                      blurRadius: 6.0)
+                ],
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(30.0),
+                child: Image(
+                  image: AssetImage(widget.destination.imageUrl),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 10.0, vertical: 40.0),
+              child: Row(
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.arrow_back),
+                    iconSize: 30.0,
+                    onPressed: () => Navigator.pop(context),
+                  )
+                ],
+              ),
+            )
+          ]),
         ],
       ),
     );
